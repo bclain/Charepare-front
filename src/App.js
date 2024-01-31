8fd8ef3c7d09d102d5bb793e36f337a92f8bb158
francine <117289190+JoyceVanelle@users.noreply.github.com>
2024-01-31 14:52:38 -0500

    css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import GaragePage from './pages/GaragePage';
+import Navbar from './components/Navbar'
 import "./styles/index.scss";
 
 function App() {
   return (
 
     <Router>
-
+      <Navbar/>
       <Routes>
 
         {/* Routes for the front */}
@@ -18,5 +19,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
